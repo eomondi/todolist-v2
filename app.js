7e2ba8bd22b25a463ead810720d66a44bd50dd66
Emmanuel <59773467+eomondi@users.noreply.github.com>
2022-05-13 00:21:49 +0300

    Modify package.json and process.env.PORT

--- app.js
+++ app.js
@@ -218,7 +218,11 @@ app.post('/delete', function(req, res) {
 
 //______________________________________________________________________________________________________________
 
-// Serve app with Express
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server is running on port 3000.");
+// Serve app
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port, function(){
+  console.log('Server has started successfully.');
 });
